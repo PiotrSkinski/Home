ac796998c77817e17606d4022bc760d22e552607
PiotrSkinski <piotr.skirzynski@szajna.pl>
2026-07-07 11:43:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "homejob-v29";
+const CACHE_NAME = "homejob-v30";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=29",
-  "./app.js?v=29",
-  "./manifest.webmanifest?v=29",
+  "./styles.css?v=30",
+  "./app.js?v=30",
+  "./manifest.webmanifest?v=30",
   "./icon.svg"
 ];
 
